5580c9eeb14ad4eebeb61f62124fc81e8fcfacff
ZofiaSzym <s30342@pjwstk.edu.pl>
2025-05-20 11:02:13 +0200

    starting

---
+++ CodeFirst/CodeFirst/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.Data;
+
+public class  AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
